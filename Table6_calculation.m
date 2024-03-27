d60b0ad803361fc042c03c965d3e4d2db213c735
EduardoMartinezEnriquez <emenriquez@tsc.uc3m.es>
2024-03-27 16:46:22 +0100

    Add files via upload

---
+++ Table6_calculation.m
@@ -0,0 +1,107 @@
+clear all
+close all
+
+% PRE IOLMaster
+load('Table6_IOLMaster700_pre.mat')
+
+nanmean(age)
+nanstd(age)
+
+nanmean(RAC_OCT)
+nanstd(RAC_OCT)
+
+nanmean(RPC_OCT)
+nanstd(RPC_OCT)
+
+nanmean(CT_OCT)
+nanstd(CT_OCT)
+
+nanmean(ACD_OCT)
+nanstd(ACD_OCT)
+
+nanmean(LT_OCT)
+nanstd(LT_OCT)
+
+nanmean(RAL_OCT)
+nanstd(RAL_OCT)
+
+nanmean(RPL_OCT)
+nanstd(RPL_OCT)
+
+nanmean(DIA_OCT)
+nanstd(DIA_OCT)
+
+nanmean(VOL_OCT)
+nanstd(VOL_OCT)
+
+nanmean(TD_OCT)
+nanstd(TD_OCT)
+
+nanmean(TM_OCT)
+nanstd(TM_OCT)
+
+clear all
+close all
+clc
+
+% POST IOLMaster
+load('Table6_IOLMaster700_post.mat')
+nanmean(age)
+nanstd(age)
+
+nanmean(RAC_OCT)
+nanstd(RAC_OCT)
+
+nanmean(RPC_OCT)
+nanstd(RPC_OCT)
+
+nanmean(CT_OCT)
+nanstd(CT_OCT)
+
+nanmean(ACD_OCT)
+nanstd(ACD_OCT)
+
+nanmean(LT_OCT)
+nanstd(LT_OCT)
+
+nanmean(RAL_OCT)
+nanstd(RAL_OCT)
+
+nanmean(RPL_OCT)
+nanstd(RPL_OCT)
+
+nanmean(TD_OCT)
+nanstd(TD_OCT)
+
+nanmean(TM_OCT)
+nanstd(TM_OCT)
+
+clear all
+close all
+
+% PRE ANTERION
+load('Table6_ANTERION_pre.mat')
+nanmean(age)
+nanstd(age)
+
+nanmean(CT_OCT)
+nanstd(CT_OCT)
+
+nanmean(ACD_OCT)
+nanstd(ACD_OCT)
+
+nanmean(LT_OCT)
+nanstd(LT_OCT)
+
+nanmean(RAL_OCT)
+nanstd(RAL_OCT)
+
+nanmean(RPL_OCT)
+nanstd(RPL_OCT)
+
+nanmean(DIA_OCT)
+nanstd(DIA_OCT)
+
+nanmean(VOL_OCT)
+nanstd(VOL_OCT)
+
